d36e383c5939a715229e1ee541e309121c3cdbeb
deniseol <denise.olsson@iths.se>
2025-05-13 16:35:29 +0200

    start

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import express from "express";
 import cors from "cors";
 import pg from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 const pool = new pg.Pool({
